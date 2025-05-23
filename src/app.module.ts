0f4e3eb1c426b8098a318f98568baae5151e300f
Alan Löffler <alan@alan.local>
2025-05-23 17:21:56 -0300

    feat(global): add ConfigModule and TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { DataSource } from "typeorm";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Admin } from "./admin/entities/admin.entity";
+import { AdminModule } from "./admin/admin.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true,
+      cache: true,
+    }),
+    AdminModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT") || 5432,
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        entities: [Admin],
+        synchronize: true, // WARNING: Disable synchronization in production
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
